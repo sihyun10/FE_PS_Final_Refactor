1713aa896291e030c23233a97df761b6a59a03ef
05Castle <kos4005@gmail.com>
2023-06-02 00:18:11 +0900

    move: CancelButton, LoadingBar, SpinnerButton 디렉토리 이동
    CancelButton, LoadingBar, SpinnerButton은 UploadPDF에서만 사용되는
    컴포넌트이므로 common 디렉토리에서 Pdf 디렉토리로 이

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,6 +1,3 @@
 export { default as PrimaryButton } from './PrimaryButton';
-export { default as SpinnerButton } from './SpinnerButton';
-export { default as PrimaryModal } from './PrimaryModal';
-export { default as LoadingBar } from './LoadingBar';
 
-export { default as CancelButton } from './CancelButton';
+export { default as PrimaryModal } from './PrimaryModal';
