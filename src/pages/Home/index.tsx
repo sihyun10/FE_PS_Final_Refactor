716a2d7689ebc5f4509f2bf2555cf85ec4cad64b
05Castle <kos4005@gmail.com>
2023-06-02 01:53:24 +0900

    split: Home.tsx를 index.tsx와 style.ts로 분리
    css를 style.ts로 분리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
+import { HomeLayout, HomeTitleContainer, Title } from './style';
 import logo from '@/assets/logo.png';
 import { CommonButton } from '@/components/common';
 
@@ -34,46 +34,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const HomeLayout = styled.div`
-  background: linear-gradient(180deg, #e8eaf6 0%, rgba(255, 255, 255, 0) 100%);
-  background-size: auto 555px;
-  background-repeat: no-repeat;
-  min-height: calc(100vh - 250px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 140px;
-  img {
-    width: 80px;
-    height: 50px;
-    margin-bottom: 40px;
-  }
-`;
-
-const HomeTitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 85px;
-`;
-
-const Title = styled.span`
-  text-align: center;
-  &.mainTitle {
-    margin-bottom: 40px;
-    font-weight: 700;
-    font-size: 80px;
-    color: #333333;
-    line-height: 100px;
-    .secondaryTheme {
-      color: #ccac55;
-    }
-  }
-  &.subTitle {
-    font-weight: 400;
-    font-size: 18px;
-    color: #5c5c5c;
-    line-height: 25px;
-  }
-`;
